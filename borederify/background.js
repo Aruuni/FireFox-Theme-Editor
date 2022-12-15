c0b3fda7c70d11604e1a4fe0c4fc75e57c42a95b
mihai <mihaifcp44279@gmail.com>
2022-12-15 23:21:44 +0000

    first

---
+++ borederify/background.js
@@ -0,0 +1,8 @@
+browser.theme.update({
+    "images": {
+        "additional_backgrounds": ["fort.jpg"]
+    },
+    "properties": {
+        "additional_backgrounds_tiling": ["repeat-x"]
+    },
+});
