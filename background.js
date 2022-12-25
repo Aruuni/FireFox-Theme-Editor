9abee015c115f3392564aa70f89aef0460fb1c0d
mihai <mihaifcp44279@gmail.com>
2022-12-25 20:08:21 +0000

    big changes

--- background.js
+++ background.js
@@ -1,8 +1,21 @@
-browser.theme.update({
-    "images": {
-        "additional_backgrounds": ["fort.jpg"]
-    },
-    "properties": {
-        "additional_backgrounds_tiling": ["repeat-x"]
-    },
+var elements = document.querySelectorAll('*');
+
+elements.forEach(function(element) {
+  console.log(element);
+});
+const element = document.getElementById('colorpicker');
+console.log(element);
+document.addEventListener("DOMContentLoaded", function() { 
+    document.querySelector('input[type="color"]').addEventListener('change', function(event) {
+        event.preventDefault();
+        console.log(event.target.value)
+        brower.theme.update({
+            "theme":{
+                "colors":{
+                    "toolbar": event.target.value
+                }
+            }
+        });
+    });
+
 });
